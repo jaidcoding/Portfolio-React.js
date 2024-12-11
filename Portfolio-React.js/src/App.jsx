94ca13d7b835bf794a06fa39e5c76f47a6576b80
Jaidon Clinton <jaidcoding@gmail.com>
2024-12-10 19:26:21 -0600

    refactor Navbar component and update background styles in App

--- Portfolio-React.js/src/App.jsx
+++ Portfolio-React.js/src/App.jsx
@@ -7,14 +7,14 @@ import Tech from "./components/Tech";
 function App() {
   return (
     <>
-      <div className="fixed -z-10 min-h-screen w-full bg-custom-gradient"></div>
+      <div class="fixed z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      
       <main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
         <Navbar />
         <Hero />
         <Tech />
         <Projects />
         <Contact />
-        <div className="text-red-500">Tailwind CSS is working!</div>
       </main>
     </>
   );
--- Portfolio-React.js/src/components/Navbar.jsx
+++ Portfolio-React.js/src/components/Navbar.jsx
@@ -1,7 +1,94 @@
+import { useState } from 'react';
+import { BsGithub, BsTwitterX, BsYoutube } from 'react-icons/bs';
+import { BiX, BiMenu } from 'react-icons/bi';
 
 const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const menuOpen = () => {
+        setIsOpen(!isOpen);
+    }
+
   return (
-    <div>Navbar</div>
+    <nav className="fixed top-0 z-10 flex w-full items-center justify-between border-b border-b-gray-700 bg-black/70 px-16 py-6 text-white backdrop-blur-md md:justify-evenly">
+        <a href="#home" className="bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent opacity-80 text-3xl font-semibold transition-all duration-300 hover:opacity-100">Jaidon Clinton</a>
+
+        <ul className="hidden md:flex gap-10">
+            <a href="#home" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+                <li>Home</li>
+            </a>
+
+            <a href="#tech" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+                <li>Tech</li>
+            </a>
+
+            <a href="#projects" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+                <li>Projects</li>
+            </a>
+
+            <a href="#contact" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+                <li>Contact</li>
+            </a>
+        </ul>
+
+        <ul className="hidden md:flex gap-5">
+            <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-red-500 hover:opacity-100">
+                <BsYoutube />
+            </li>
+
+            <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-orange-500 hover:opacity-100">
+            <BsGithub />
+            </li>
+
+            <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-blue-500 hover:opacity-100">
+            <BsTwitterX />
+            </li>
+        </ul>
+
+        {isOpen ? (
+            <BiX className="block md:hidden text-4xl" onClick={menuOpen}/>
+        ) : (
+            <BiMenu className="block md:hidden text-4xl" onClick={menuOpen}/>
+        )}
+
+        {isOpen && (
+            <div className={`fixed right-0 top-[84px] flex h-screen width-1/2 flex-col items-start justify-start gap-10 border-l border-gray-800 bg-black/90 p-12 ${isOpen ? "block" : "hidden"}`}>
+
+                <ul className='flex flex-col gap-8'>
+                        <a href="#home" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+                        <li>Home</li>
+                    </a>
+
+                    <a href="#tech" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+                        <li>Tech</li>
+                    </a>
+
+                    <a href="#projects" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+                        <li>Projects</li>
+                    </a>
+
+                    <a href="#contact" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+                        <li>Contact</li>
+                    </a>
+                </ul>
+                <ul className="flex flex-wrap gap-5">
+                    <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-red-500 hover:opacity-100">
+                        <BsYoutube />
+                    </li>
+
+                    <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-orange-500 hover:opacity-100">
+                        <BsGithub />
+                    </li>
+
+                    <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-blue-500 hover:opacity-100">
+                        <BsTwitterX />
+                    </li>
+                </ul>
+            </div>
+        )}
+
+    </nav>
   )
 }
 
